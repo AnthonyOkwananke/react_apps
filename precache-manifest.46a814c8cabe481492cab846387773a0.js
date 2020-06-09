52e5111d8e8788adf995d1bb022b46066037981d
Anthony Okwananke <826717@ACDM.DS.SAIT.CA>
2020-06-09 10:04:55 -0600

    Updates

---
+++ precache-manifest.46a814c8cabe481492cab846387773a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3983c283a4d06ffe76a54d24f9c2adea",
+    "url": "/react_apps/index.html"
+  },
+  {
+    "revision": "a526c88575095352588a",
+    "url": "/react_apps/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5f6d654cf7c4acaa2809",
+    "url": "/react_apps/static/js/2.c3db40cf.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/react_apps/static/js/2.c3db40cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a526c88575095352588a",
+    "url": "/react_apps/static/js/main.d6f04ca0.chunk.js"
+  },
+  {
+    "revision": "e2e60f838998e7171459",
+    "url": "/react_apps/static/js/runtime-main.5a2d8f43.js"
+  }
+]);
